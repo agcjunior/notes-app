69c76a0cdda7a1d47c54a94242abf1138fe47f4b
antonio.junior_wipro <agcjunior@gmail.com>
2026-04-24 14:49:08 -0300

    Notes App setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  
+  return (
+    <div className="max-w-lg mx-auto mt-15 p-6 bg-gray-300 rounded-lg">
+      <h2 className="text-2xl font-bold mb-4 text-center">Notes App</h2>
+    </div>
+  )
+}
+
+export default App
