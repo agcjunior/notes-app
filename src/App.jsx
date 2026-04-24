90a339642a50100fed54bc89705c3ecde1e45607
antonio.junior_wipro <agcjunior@gmail.com>
2026-04-24 17:52:11 -0300

    NoteForm implementation (1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import NoteForm from "./components/NoteForm"
 function App() {
   
   return (
     <div className="max-w-lg mx-auto mt-15 p-6 bg-gray-300 rounded-lg">
-      <h2 className="text-2xl font-bold mb-4 text-center">Notes App</h2>
+      <h2 className="text-2xl font-bold mb-4 text-center">📝Notes App</h2>
+      <NoteForm />
     </div>
   )
 }
